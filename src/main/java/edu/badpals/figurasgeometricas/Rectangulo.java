fa10e5cee1a30bdc748cd13dae4c0b367837649a
AmarinoLama <amarinolama@gmail.com>
2024-05-09 12:22:24 +0200

    feat (Rectangulo) : add attributes

--- src/main/java/edu/badpals/figurasgeometricas/Rectangulo.java
+++ src/main/java/edu/badpals/figurasgeometricas/Rectangulo.java
@@ -1,4 +1,13 @@
 package edu.badpals.figurasgeometricas;
 
 public class Rectangulo {
+
+    public double base;
+    public double altura;
+
+    public Rectangulo(double base, double altura) {
+        /*super("Rectangulo")*/;
+        this.base = base;
+        this.altura = altura;
+    }
 }
