2aedbbf8a721e387140d8bec9e31e14fa68a1b25
AmarinoLama <amarinolama@gmail.com>
2024-05-09 12:10:05 +0200

    test (CirculoTest) : add tests

--- src/test/java/edu/badpals/figurasgeometricas/CirculoTest.java
+++ src/test/java/edu/badpals/figurasgeometricas/CirculoTest.java
@@ -1,7 +1,22 @@
 package edu.badpals.figurasgeometricas;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CirculoTest {
 
+    @Test
+    void testCalcularArea() {
+        Circulo circulo = new Circulo(5);
+        circulo.calcularArea();
+        assertEquals(78.54, circulo.area, 0.01);
+    }
+
+    @Test
+    void testCalcularAreaRadioCero() {
+        Circulo circulo = new Circulo(0);
+        circulo.calcularArea();
+        assertEquals(0, circulo.area, 0.01);
+    }
 }
