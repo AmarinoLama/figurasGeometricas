51a32b281b06d671d0a3f06a3c42a62b116e1ef7
AmarinoLama <amarinolama@gmail.com>
2024-05-09 12:26:45 +0200

    feat (Rectangulo) : make class extends abstract class

--- src/main/java/edu/badpals/figurasgeometricas/Rectangulo.java
+++ src/main/java/edu/badpals/figurasgeometricas/Rectangulo.java
@@ -1,13 +1,24 @@
 package edu.badpals.figurasgeometricas;
 
-public class Rectangulo {
+public class Rectangulo extends FiguraGeometrica {
 
     public double base;
     public double altura;
 
     public Rectangulo(double base, double altura) {
-        /*super("Rectangulo")*/;
+        super();
         this.base = base;
         this.altura = altura;
+    }
+
+    public Rectangulo(String nombre, double base, double altura) {
+        super(nombre);
+        this.base = base;
+        this.altura = altura;
+    }
+
+    @Override
+    public void calcularArea() {
+
     }
 }
