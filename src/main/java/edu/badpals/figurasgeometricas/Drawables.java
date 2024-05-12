1cb2729a8d9959355022621c914cbebcd16293a6
AmarinoLama <amarinolama@gmail.com>
2024-05-12 16:59:36 +0200

    feat (Drawables) : make method static

--- src/main/java/edu/badpals/figurasgeometricas/Drawables.java
+++ src/main/java/edu/badpals/figurasgeometricas/Drawables.java
@@ -3,7 +3,7 @@ package edu.badpals.figurasgeometricas;
 import java.util.ArrayList;
 
 public class Drawables {
-    public void dibujarDrawables(ArrayList<Drawable> figuras){
+    public static void dibujarDrawables(ArrayList<Drawable> figuras){
         for (Drawable figura : figuras){
             figura.draw();
         }
