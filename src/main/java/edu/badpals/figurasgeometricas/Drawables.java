4a4b0887aa2e2596515f1acf0cdb0400ac3ee26c
AmarinoLama <amarinolama@gmail.com>
2024-05-12 16:48:42 +0200

    feat (Drawables) : add method

--- src/main/java/edu/badpals/figurasgeometricas/Drawables.java
+++ src/main/java/edu/badpals/figurasgeometricas/Drawables.java
@@ -1,4 +1,11 @@
 package edu.badpals.figurasgeometricas;
 
+import java.util.ArrayList;
+
 public class Drawables {
+    public void dibujarDrawables(ArrayList<Drawable> figuras){
+        for (Drawable figura : figuras){
+            figura.draw();
+        }
+    }
 }
